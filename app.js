64d6068dfc550af8077b0d926668dea6df594437
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-05 16:06:07 +0300

    Workplace Created

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id: 1,
+    name: "Article Name",
+    description: "Article description"
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
