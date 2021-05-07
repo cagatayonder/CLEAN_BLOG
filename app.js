481cd245fe64afd602b54c06d1308bbe7b927c25
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-07 16:15:33 +0300

    add 3 posts

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Post = require('./models/Post');
+const post = Post.find({});
 
 const app = express();
 
+// Connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 //TEMPLATE ENGİNE
-app.set("view engine","ejs"); 
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.get('/', async (req, res) => {
+  posts = await Post.find({});
+  res.render('index'),{posts};
+});
 
 app.get('/about', (req, res) => {
   res.render('about');
-})
+});
 
 app.get('/add_post', (req, res) => {
   res.render('add_post');
-})
+});
 
-app.get('/posts', (req, res) => {
-  res.render('posts');
-})
-
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
